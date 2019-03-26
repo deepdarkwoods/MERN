b6bd1aa304d1d5262f815aeec3b72c66b03d6d0d
Brad Parker <deepdarkwoods@yahoo.com>
2019-03-26 06:36:38 -0500

    Added Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const gravitar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // Load User model
 const User = require("../../models/User");
@@ -13,7 +15,60 @@ router.get("/test", (req, res) => res.json({ msg: "Users works" }));
 //@desc     Register user
 //@access   Public
 router.post("/register", (req, res) => {
-  User.findOne({ email: request.body.email });
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravitar.url("user.req.email", {
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm" //default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (error, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+//@route    GET api/users/login
+//@desc     Login user / return token
+//@access   Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    //check if user is false (didn't find the username in Mongodb)
+    if (!user) {
+      return res.status(404).json({ user: "User not found" });
+    }
+
+    //check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
+  });
 });
 
 module.exports = router;
